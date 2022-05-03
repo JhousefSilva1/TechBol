dc70b594edd73cf6f3697c85f25de255f7a5ee95
JosePozoSilva <jhosede23@gmail.com>
2022-05-03 00:54:45 -0400

    cambios en enrutamiento de firebase y backend json

---
+++ BackEnd/techbol/techbol/src/test/java/com/techbol/ucb/edu/bo/techbol/TechbolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbol.ucb.edu.bo.techbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

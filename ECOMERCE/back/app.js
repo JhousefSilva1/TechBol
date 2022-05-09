5f5377bd4c790d86e706b1de4f921d38411c4b30
JosePozoSilva <jhosede23@gmail.com>
2022-05-08 23:48:49 -0400

    setting backend port and connecting database

--- ECOMERCE/back/app.js
+++ ECOMERCE/back/app.js
@@ -1,6 +1,23 @@
 'use strict'
 
 var express = require('express');
+var app = express();
 var bodypasr = require('body-parser');
 var mongoose = require ('mongoose');
 var port = process.env.PORT || 4201;
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/BolTech',(err, res)=>{ //creamos un error y una respuesta
+    if(err){
+        console.log(err);//muestra el error
+    }
+    else{
+        console.log('Servidor corriendo');
+        app.listen(port, function(){
+            console.log('Servidor corriendo en el puerto '+port);
+        })
+    }
+})
+
+//exportando app
+module.exports = app;

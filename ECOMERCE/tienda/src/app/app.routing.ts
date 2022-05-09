a7444ed8402d3b98a35b1f61fcff26f06e095d22
JosePozoSilva <jhosede23@gmail.com>
2022-05-09 11:36:53 -0400

    agregando rutas de los componentes, estaticos

--- ECOMERCE/tienda/src/app/app.routing.ts
+++ ECOMERCE/tienda/src/app/app.routing.ts
@@ -6,9 +6,40 @@ import { AuthGuard } from "../app/guards/auth.guard";
 import { InicioComponent } from "./components/inicio/inicio.component";
 import { LoginComponent } from "./components/login/login.component";
 
+
+
+
+
+import { EmbajadorasComponent } from "./components/static/embajadoras/embajadoras.component";
+import { ImpactoComponent } from "./components/static/impacto/impacto.component";
+import { NosotrosComponent } from "./components/static/nosotros/nosotros.component";
+import { PoliticasEnvioComponent } from "./components/static/politicas-envio/politicas-envio.component";
+import { TerminosCondicionesComponent } from "./components/static/terminos-condiciones/terminos-condiciones.component";
+import { ContactoComponent } from "./components/static/contacto/contacto.component";
+import { NotfoundComponent } from "./components/notfound/notfound.component";
+//import { AuthGuard } from "../app/guards/auth.guard";
+
+
 const appRoute : Routes = [
     {path: '', component: InicioComponent},
     {path: 'login', component: LoginComponent},
+
+
+
+
+
+
+
+
+
+
+    {path: 'contacto', component: ContactoComponent},
+    {path: 'embajadoras', component: EmbajadorasComponent},
+    {path: 'impacto-social', component: ImpactoComponent},
+    {path: 'nosotros', component: NosotrosComponent},
+    {path: 'politicas-envio', component: PoliticasEnvioComponent},
+    {path: 'terminos-condiciones', component: TerminosCondicionesComponent},
+    {path: '**', component: NotfoundComponent}
 
 ]
    
